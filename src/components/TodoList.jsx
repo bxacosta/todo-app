bb1b2ba17f2e89cc0ce1eb16b2678708662c335a
Bryan Acosta <bryan-ax@hotmail.com>
2024-07-21 19:36:26 -0500

    feat: agrega funcionalidad para marcar tareas como realizadas

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,11 @@
 import {TodoItem} from "./TodoItem.jsx";
 import PropTypes from "prop-types";
 
-export function TodoList({todos}) {
+export function TodoList({todos, toggleTodo}) {
     return (
         <ul>
             {todos.map((todo) => (
-                <TodoItem key={todo.id} todo={todo}/>
+                <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo}/>
             ))}
         </ul>
     );
@@ -18,5 +18,6 @@ TodoList.propTypes = {
             text: PropTypes.string.isRequired,
             completed: PropTypes.bool.isRequired
         })
-    ).isRequired
+    ).isRequired,
+    toggleTodo: PropTypes.func.isRequired
 };
